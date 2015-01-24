52a5f0c405c8382de36229f965600b41c14af9fb
Kornel Lesiński <kornel@geekhood.net>
2015-01-24 23:40:17 +0000

    Add canary files to detect when out_dir is cleaned

--- src/build.sh
+++ src/build.sh
@@ -1,7 +1,16 @@
 set -e
 cd "$CARGO_MANIFEST_DIR/mozjpeg"
-test -f ./configure || autoreconf -i
-test -f ./Makefile || ./configure --prefix="$OUT_DIR" --host="$HOST" --build="$TARGET" --enable-static --disable-shared --without-arith-enc --without-arith-dec --without-turbojpeg
+test -f "$OUT_DIR/autoreconfdone" || {
+    autoreconf -i && touch "$OUT_DIR/autoreconfdone";
+}
+test -f "$OUT_DIR/confdone" || {
+    ./configure --prefix="$OUT_DIR" --host="$HOST" --build="$TARGET" --enable-static --disable-shared --without-arith-enc --without-arith-dec --without-turbojpeg &&
+    touch "$OUT_DIR/confdone";
+}
 make -j"$NUM_JOBS" install
 echo "cargo:rustc-flags=-l static=jpeg -L native=$OUT_DIR/lib"
-echo "cargo:libdir=$OUT_DIR/lib"
+test -f $OUT_DIR/lib/libjpeg.a || {
+    echo "make failed to generate lib; pwd=$PWD out=$OUT_DIR";
+    ls -laR "$OUT_DIR"
+    exit 1;
+}
