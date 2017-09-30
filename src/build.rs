301c084b337343df0b02e535d17a15eca7a0788d
Kornel <kornel@geekhood.net>
2017-09-30 16:11:30 +0100

    Diagnostics

--- src/build.rs
+++ src/build.rs
@@ -1,13 +1,15 @@
 fn main() {
-    let num_jobs = std::env::var("NUM_JOBS").unwrap();
-    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let num_jobs = std::env::var("NUM_JOBS").expect("NUM_JOBS");
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR");
 
     let mut cmd = std::process::Command::new("make");
     cmd.arg("-j").arg(num_jobs);
 
-    if !cmd.status().unwrap().success() {
-        panic!("Script failed");
+    match cmd.status() {
+        Ok(_) => {},
+        Err(e) => {
+            panic!("Failed to launch make: {}", e);
+        },
     }
-
     println!("cargo:rustc-flags=-L {}", out_dir);
 }
