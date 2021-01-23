7d7ee445d60ac953ef8ec2da8e33175879498364
Kornel <kornel@geekhood.net>
2021-01-23 13:31:10 +0000

    C's getenv is not thread-safe, thus a danger for Rust

--- src/build.rs
+++ src/build.rs
@@ -101,6 +101,7 @@ fn main() {
         #define LIBJPEG_TURBO_VERSION 0
         #define BITS_IN_JSAMPLE 8
         #define STDC_HEADERS 1
+        #define NO_GETENV 1
         #define HAVE_STDLIB_H 1
         #define HAVE_UNSIGNED_CHAR 1
         #define HAVE_UNSIGNED_SHORT 1
