dd21d91b6bedc9ba3530db6a67a10de0c4a8e31c
Kornel <kornel@geekhood.net>
2021-03-12 14:46:36 +0000

    Rename test fn

--- tests/dc.rs
+++ tests/dc.rs
@@ -4,7 +4,7 @@ use cloudflare_soos::jpeg::*;
 use mozjpeg_sys::*;
 use std::mem;
 
-fn decode_rgb(file_name: &str) -> (Vec<u8>, u32, u32) {
+fn decode_rgb_file(file_name: &str) -> (Vec<u8>, u32, u32) {
     unsafe {
         let mut err: jpeg_error_mgr = mem::zeroed();
         let mut cinfo: jpeg_decompress_struct = mem::zeroed();
@@ -15,13 +15,21 @@ fn decode_rgb(file_name: &str) -> (Vec<u8>, u32, u32) {
         let mode = CString::new("rb").unwrap();
         let fh = libc::fopen(file_name.as_ptr(), mode.as_ptr());
         jpeg_stdio_src(&mut cinfo, fh);
-        jpeg_read_header(&mut cinfo, true as boolean);
+        let res = decode_rgb_cinfo(&mut cinfo);
+        libc::fclose(fh);
+        res
+    }
+}
+
+fn decode_rgb_cinfo(cinfo: &mut jpeg_decompress_struct) -> (Vec<u8>, u32, u32) {
+    unsafe {
+        jpeg_read_header(cinfo, true as boolean);
 
         let width = cinfo.image_width;
         let height = cinfo.image_height;
 
         cinfo.out_color_space = J_COLOR_SPACE::JCS_RGB;
-        jpeg_start_decompress(&mut cinfo);
+        jpeg_start_decompress(cinfo);
         let row_stride = cinfo.image_width as usize * cinfo.output_components as usize;
         let buffer_size = row_stride * cinfo.image_height as usize;
         let mut buffer = vec![0u8; buffer_size];
@@ -29,12 +37,11 @@ fn decode_rgb(file_name: &str) -> (Vec<u8>, u32, u32) {
         while cinfo.output_scanline < cinfo.output_height {
             let offset = cinfo.output_scanline as usize * row_stride;
             let mut jsamparray = [buffer[offset..].as_mut_ptr()];
-            jpeg_read_scanlines(&mut cinfo, jsamparray.as_mut_ptr(), 1);
+            jpeg_read_scanlines(cinfo, jsamparray.as_mut_ptr(), 1);
         }
 
-        jpeg_finish_decompress(&mut cinfo);
-        jpeg_destroy_decompress(&mut cinfo);
-        libc::fclose(fh);
+        jpeg_finish_decompress(cinfo);
+        jpeg_destroy_decompress(cinfo);
 
         (buffer, width, height)
     }
@@ -79,13 +86,13 @@ fn encode_rgb(buffer: &[u8], width: u32, height: u32, quality: i32) -> Vec<u8> {
 
 #[test]
 fn no_green_faces() {
-    let (buffer, width, height) = decode_rgb("tests/test.jpg");
+    let (buffer, width, height) = decode_rgb_file("tests/test.jpg");
     let enc = encode_rgb(&buffer, width, height, 60);
     assert!(enc.len() > 20000);
     let m = Decoder::new(&mut enc.as_slice()).decode().unwrap();
     let f = m.into_iter().map(|m| m.marker)
-        .filter_map(|m| match m { Scan(f) => Some(f), _ => None})
-        .next().unwrap();
+        .find_map(|m| match m { Scan(f) => Some(f), _ => None})
+        .unwrap();
     assert_eq!(3, f.component_indices.len());
     assert_eq!(3, f.dc_table_indices.len());
 }
